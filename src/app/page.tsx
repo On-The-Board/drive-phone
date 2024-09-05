40eec2e276965da07ba4ef0b285290b3a65baffb
DashOTB <capo@dashotb.com>
2024-09-05 02:43:23 +0200

    nice

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import bp from "@/img/brokenPhone.png"
+import rp from "@/img/Reparateur.png"
 
 export default function Home() {
   return (
@@ -8,16 +9,21 @@ export default function Home() {
         <div className="w-full flex flex-row">
           <Image src={bp} alt="tel cassé" className="w-56 absolute" />
           <div className="flex flex-col pt-20 ml-40">
-            <h1 className="font-bold text-lg">TÉLÉPHONE CASSÉ ?</h1>
+            <h1 className="font-bold text-lg text-end">TÉLÉPHONE CASSÉ ?</h1>
             <p className="text-sm text-end leading-5 font-light">On se déplace chez vous <br />pour le réparer en toute <br />tranquilité</p>
             <button className="w-20 rounded-sm bg-white h-6 mt-4 self-end text-blue-600 text-xs">
               Réserver
             </button>
           </div>
         </div>
-        <div className="w-full bg-white h-80 rounded-md mt-20 mx-24 opacity-45 self-center">
-          <h1 className="text-white absolute">test</h1>
+        <div className="w-72 bg-white bg-opacity-45 h-[21rem] rounded-md mt-20 mx-24 self-center flex flex-col">
+          <Image src={rp} alt="reparation smartphone" className="w-52 h-fit pt-6 self-center"/>
+          <h1 className="self-center py-3">Réparation Smartphone</h1>
+          <p className="text-xs px-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati ipsum expedita omnis optio molestiae laboriosam mollitia ea, eligendi excepturi amet, repellat sed natus in neque vero vitae, disti...</p>
         </div>
+      </div>
+      <div className="h-screen bg-white">
+
       </div>
     </main>
   );
