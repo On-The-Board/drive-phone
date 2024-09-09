2c6033840aa6048b6795a30f3fe84effa7c3a082
DashOTB <capo@dashotb.com>
2024-09-10 01:55:23 +0200

    :clown_face: Correction des Erreurs de Mattis

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,10 +1,8 @@
 "use client";
 // import Openrouteservice from "openrouteservice-js";
 // import Leaflet from "leaflet";
-import * as ReactLeaflet from "react-leaflet";
-import "leaflet/dist/leaflet.css";
-
-const { MapContainer, TileLayer, Popup, Marker } = ReactLeaflet;
+import { MapContainer, TileLayer, Popup, Marker } from "react-leaflet";
+import "leaflet/dist/leaflet.css"
 
 interface MapProps {
   start: [number, number]; // GPS Position
