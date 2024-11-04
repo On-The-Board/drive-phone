54f0085849260ed2495b4a0baa78ced66462befc
DashOTB <capo@dashotb.com>
2024-11-04 08:30:21 +0100

    :+1:

--- src/app/account/login/page.tsx
+++ src/app/account/login/page.tsx
@@ -1,9 +1,13 @@
 import { LoginForm } from "./form";
+import { Suspense } from 'react'
+
 
 export default function LoginPage() {
     return (
-        <main className="min-h-screen px-5 lg:px-36 text-black">
-            <LoginForm />
-        </main>
+        <Suspense>
+            <main className="min-h-screen px-5 lg:px-36 text-black">
+                <LoginForm />
+            </main>
+        </Suspense>
     );
 }
