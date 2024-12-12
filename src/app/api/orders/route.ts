3969b83da82e73571397053ba434d5783aa193bc
DashOTB <capo@dashotb.com>
2024-12-12 16:51:42 +0100

    Update route.ts

--- src/app/api/orders/route.ts
+++ src/app/api/orders/route.ts
@@ -2,6 +2,7 @@ import { prisma } from "@/lib/prisma";
 import { NextResponse, NextRequest } from "next/server";
 import type { NextApiRequest, NextApiResponse } from "next"
 
+export const dynamic = 'force-dynamic'
 
 export async function GET(request: Request) {
     const meetings = await prisma.order.findMany();
